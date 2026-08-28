9197b59ae7d33f03bcfa05c17b5b2e07b0b5fca5
José Luis <jlrodriguez0906@gmail.com>
2026-08-28 17:11:51 -0500

    [ADD] Se añade template

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -5,17 +5,7 @@ namespace App\Controllers;
 class Home extends BaseController
 {
     public function index(): string
-    {
-        return view('welcome_message');
-    }
-
-    public function saludo($nombre, $apellido){
-        echo "Hola " . $nombre . " " . $apellido;
-    }
-
-    public function sumita($n1, $n2){
-        $suma =$n1 + $n2;
-        echo "El resultado es: $suma"; 
-    } 
-
+    {        
+        return view('facturacion/index');
+    }    
 }
