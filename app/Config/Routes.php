637121bf841fbd445702c81b527aeecd88c65275
José Luis <jlrodriguez0906@gmail.com>
2026-08-28 16:29:56 -0500

    [ADD] Se añade AdminLTE

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -4,3 +4,11 @@ use CodeIgniter\Router\RouteCollection;
 
 /** @var RouteCollection $routes */
 $routes->get('/', 'Home::index');
+
+$routes->get('/saludo/(:any)/(:any)', 
+    'Home::saludo/$1/$2', ['as' => 'saludo']);
+
+$routes->get('/sumar/(:num)/(:num)', 
+    'Home::sumita/$1/$2', ['as' => 'sumita']);
+
+$routes->get('/prueba', 'Prueba::index');
--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -8,4 +8,14 @@ class Home extends BaseController
     {
         return view('welcome_message');
     }
+
+    public function saludo($nombre, $apellido){
+        echo "Hola " . $nombre . " " . $apellido;
+    }
+
+    public function sumita($n1, $n2){
+        $suma =$n1 + $n2;
+        echo "El resultado es: $suma"; 
+    } 
+
 }
